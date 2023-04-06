811269a8e7b18c2b01a45824f010aa9cc441da94
Mohammad Farseen Manekhan <67449931+farseenmanekhan1232@users.noreply.github.com>
2023-04-06 11:25:05 -0700

    Delete firebase_keys.js

--- firebase_keys.js
+++
@@ -1,9 +0,0 @@
-const keys = {
-  apiKey: "AIzaSyDYVDhzQhhAFOoXpcILgFIl4_e1YeSrsXc",
-  authDomain: "react-auth-f6c73.firebaseapp.com",
-  projectId: "react-auth-f6c73",
-  storageBucket: "react-auth-f6c73.appspot.com",
-  messagingSenderId: "56261411508",
-  appId: "1:56261411508:web:eb9568c437ae1ba9020de1",
-};
-export default keys;
